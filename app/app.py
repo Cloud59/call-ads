2e92df46f7885ef3eec90de056f176b13cf64b30
Paul Hallett <hello@phalt.co>
2014-03-24 11:07:59 +0000

    Added the control centre

--- app/app.py
+++ app/app.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
+@app.route('/control/')
+def controller():
+    return render_template('control.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
